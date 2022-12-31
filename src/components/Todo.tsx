a4d2df562e8e0d77c752eeb17b8132250df581a8
Alex Moreira <alex.moreira.andrade@hotmail.com>
2022-12-31 01:06:04 -0300

    Feature: Component Todo and styles

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,19 +1,61 @@
 import { PlusCircle } from 'phosphor-react'
 
+import Clipboard from '../assets/Clipboard.svg';
 
 import styles from './Todo.module.scss';
+import { TodoList } from './TodoList';
+// import { TodoList } from './TodoList';
 
+import { useState } from 'react';
+
+interface Todo {
+    text: string;
+    state: 'complete' | 'pending';
+}
 
 export function Main() {
+
+    const [todos, setTodos] = useState<Todo[]>([
+        {
+            text: 'Lorem ipsum dolor sit amet consectet mollitia aliquam error illo ipsum. Beatae, nobis! Iure asperiores hic praesentium.',
+            state: 'complete'
+        },
+        {
+            text: 'Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.',
+            state: 'pending'
+        }
+
+    ]);
+    const [todoText, setTodoText] = useState('');
+
+    async function handleCreateNewTodo() {
+
+        const todoCreate: Todo = {
+            text: todoText,
+            state: 'pending'
+        }
+
+        setTodos([
+            ...todos,
+            todoCreate
+        ])
+
+        setTodoText('');
+    }
+
+    // console.log(todos)
+
     return (
         <>
             <div className={styles.createTodo}>
                 <input
                     type="text"
                     placeholder='Adicione uma nova tarefa'
+                    value={todoText}
+                    onChange={event => setTodoText(event.target.value)}
                 />
 
-                <button title='Criar'>
+                <button title='Criar' onClick={handleCreateNewTodo} >
                     Criar
                     <PlusCircle size={20} />
                 </button>
@@ -22,32 +64,37 @@ export function Main() {
             <div className={styles.listTodo}>
 
                 <div className={styles.infoTodo}>
-                    <p className={styles.textOne} >Tarefas criadas <span> 0 </span> </p>
+                    <p className={styles.textOne} >Tarefas criadas <span> {todos.length} </span> </p>
                     <p className={styles.textTwo} >Concluídas <span> 2 de 5</span> </p>
                 </div>
 
-                <ul>
-                    <li>
-                        <button>complete</button>
-                        Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.
-                        <button>Delete</button>
-                    </li>
-                    <li>
-                        <button>complete</button>
-                        Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.
-                        <button>Delete</button>
-                    </li>
-                    <li>
-                        <button>complete</button>
-                        Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.
-                        <button>Delete</button>
-                    </li>
-                    <li>
-                        <button>complete</button>
-                        Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.
-                        <button>Delete</button>
-                    </li>
-                </ul>
+                {
+                    todos.length != 0 ?
+                        <div className={styles.todoList}>
+                            <ul>
+                                {todos.map(todo => (
+                                    <TodoList
+                                        key={todo.text}
+                                        text={todo.text}
+                                        state={todo.state}
+                                    />
+                                ))}
+                            </ul>
+                        </div>
+                        :
+                        <div className={styles.todoListEmpty}>
+                            <div className={styles.todoListEmptyContent}>
+                                <div className={styles.divImg}>
+                                    <img src={Clipboard} alt="" />
+                                </div>
+                                <p>
+                                    Você ainda não tem tarefas cadastradas
+                                    <span>Crie tarefas e organize seus itens a fazer</span>
+                                </p>
+                            </div>
+                        </div>
+                }
+
 
             </div>
 
